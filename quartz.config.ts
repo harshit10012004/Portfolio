10c742af40df355f6c1011b1f51b3223c0e77995
harshit10012004 <harshitynr8@gmail.com>
2026-03-09 21:43:32 +0530

    changes

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "harshit10012004.github.io/Portfolio",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
